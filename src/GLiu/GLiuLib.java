a09ff89d03857051a0cee2543f54ae631410464d
BT_1N3_16 <gliu3637@bths.edu>
2018-09-18 10:14:37 -0400

    End of class update

--- src/GLiu/GLiuLib.java
+++ src/GLiu/GLiuLib.java
@@ -1,8 +1,19 @@
 package GLiu;
 
 public class GLiuLib {
-    public static void println(String str)
+    public static void println(String dstr)
     {
-       System.out.println("\"Print something\"");
+        String mm="01";
+        String dd="30";
+        String yyyy="2000";
+      dstr=(mm+"/"+dd+"/"+yyyy);
+      int index=dstr.indexOf("/");
+      String month=dstr.substring(0,index);
+      String day=dstr.substring(index+1);
+
+      String dayend=day.substring(0,index);
+      String year=day.substring(index+1,day.length());
+      String fin=dayend+"-"+month+"-"+year;
+      System.out.println(fin);
     }
 }
