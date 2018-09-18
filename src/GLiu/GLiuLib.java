cc6fa7a9570a9571fcb708233efdc9e572c5528c
BT_1N3_16@bktech.local <BT_1N3_16@bktech.local>
2018-09-18 09:16:38 -0400

    Initial commit

---
+++ src/GLiu/GLiuLib.java
@@ -0,0 +1,8 @@
+package GLiu;
+
+public class GLiuLib {
+    public static void println(String str)
+    {
+       System.out.println("\"Print something\"");
+    }
+}
---
+++ src/GLiu/TestFile.java
@@ -0,0 +1,10 @@
+package GLiu;
+
+import static GLiu.GLiuLib.println;
+
+public class TestFile {
+    public static void main(String[] args)
+    {
+        println("Just print something");
+    }
+}
