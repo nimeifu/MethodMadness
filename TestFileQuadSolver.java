6ba8fd30906fe2716cd5a65c39c0fb31678ad198
Genjie <42303447+nimeifu@users.noreply.github.com>
2018-09-23 23:27:09 -0400

    Add files via upload

---
+++ TestFileQuadSolver.java
@@ -0,0 +1,12 @@
+package GLiu;
+import GLiu.quadSolver.quad;
+
+public class TestFileQuadSolver {
+        public static void main(String[] args)
+        {
+
+            double a=4;
+            double b=5;
+            double c=4;
+        }
+}
